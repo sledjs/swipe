7450d4bfade10917841762a47009fa2def2c1769
rascada <carftjr@gmail.com>
2016-02-11 21:54:00 +0100

    universal boostrapEvents

--- src/Swipe.js
+++ src/Swipe.js
@@ -6,32 +6,34 @@ class Swipe {
     this.press = false;
   }
 
+  init(core) {
+    this.slides = core.modules.slides;
+
+    this.bootstrapEvents(core.$, 'mousemove', 'mousedown', 'mouseup', 'mouseleave');
+  }
+
+  bootstrapEvents($elem, move, down, ...up) {
+    $elem.addEventListener(move, e => ::this.swipe(e.movementX));
+    $elem.addEventListener(down, this.toggle(true));
+    up.forEach(event =>
+      $elem.addEventListener(event, this.toggle(false)));
+  }
+
+  swipe(movement) {
+    if (this.press && Math.abs(movement) > this.drag)
+      this.slides[movement < 0 ? 'next' : 'prev']();
+  }
+
   toggle(bool) {
     bool = typeof bool == 'boolean'
       ? bool
       : this.press = !this.press;
 
     this.press = bool;
-
     return e => {
       this.toggle(bool);
-
       return false;
     };
-  }
-
-  init(core) {
-    this.slides = core.modules.slides;
-
-    core.$.addEventListener('mousemove', e => ::this.swipe(e.movementX));
-    core.$.addEventListener('mousedown', this.toggle(true));
-    core.$.addEventListener('mouseup', this.toggle(false));
-    core.$.addEventListener('mouseleave', this.toggle(false));
-  }
-
-  swipe(movement) {
-    if (this.press && Math.abs(movement) > this.drag)
-      this.slides[movement < 0 ? 'next' : 'prev']();
   }
 };
 
